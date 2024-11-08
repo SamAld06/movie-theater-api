79eb4d4cfcd8718304631655035130f0cd917e2b
SamAld06 <samgaldred@gmail.com>
2024-11-08 12:53:17 +0000

    added 404 status cod for user router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,17 +5,26 @@ const{check, validationResult, matchedData} = require("express-validator")
 
 router.get("/users", async function (req,res) {
     const users = await User.findAll()
+    if (!users) {
+        return res.status(404).json({message: "users not found"})
+    }
     res.json(users)
 })
 
 router.get("/users/:id", async function (req, res) {
     const user = await User.findByPk(req.params.id)
+    if (!user) {
+        return res.status(404).json({message: "user not found"})
+    }
     res.json(user)
 })
 
 router.get("/users/:id/shows", async function (req, res) {
     const user = await User.findByPk(req.params.id)
     const userShows = await Show.findAll(user)
+    if (!user || !userShows) {
+        return res.status(404).json({message: "user or user shows not found"})
+    }
     res.json(userShows)
 })
 
